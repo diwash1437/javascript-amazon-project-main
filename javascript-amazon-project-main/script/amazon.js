9110f2968c48c87a45d4097bbfa7e96b9bb73238
diwashDev <diwash@dev>
2025-07-14 16:16:41 +0900

    add product.js file

--- javascript-amazon-project-main/script/amazon.js
+++ javascript-amazon-project-main/script/amazon.js
@@ -1,34 +1,4 @@
-const products = [
-    {
-        Image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating: {
-            star: 4.5,
-            count: 87,
-        },
-        priceCent: 1090
 
-    },
-    {
-        Image: 'images/products/intermediate-composite-basketball.jpg',
-        name: 'Intermediate Size Basketball',
-        rating: {
-            star: 4,
-            count: 127,
-        },
-        priceCent: 2095
-    },
-    {
-        Image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating: {
-            star: 4.5,
-            count: 56,
-        },
-        priceCent: 799
-    }
-
-]
 
 
 // console.log(products);
